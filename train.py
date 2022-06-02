61fd3434ea302a2f8922d4a656466f9b076c3946
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-02 09:01:28 -0500

    Added sample training with linear networks

---
+++ train.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from dataset import xor_sampler
+
+epochs = 1000
+batch_size = 64
+
+model = nn.Sequential(
+    nn.Linear(2, 1),
+    nn.Sigmoid(),
+)
+
+optimizer = optim.Adam(model.parameters())
+criterion = nn.BCELoss()
+
+losses = []
+for epoch in tqdm(range(epochs)):
+    x, y = xor_sampler(batch_size)
+
+    x = torch.from_numpy(x).float()
+    y = torch.unsqueeze(torch.from_numpy(y), axis=1).float()
+
+    y_hat = model(x)
+    loss = criterion(y_hat, y)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    losses.append(loss.item())
+
+fig = plt.figure()
+plt.plot(losses)
+plt.savefig('training_loss.png')
+plt.close(fig)
