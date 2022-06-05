c6c6e7cbdab9c8f9003eb14a1a02fdb58b923876
César Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-05 14:26:56 -0500

    Moved normal convnet model to model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class LoopNet(nn.Module):
     def __init__(self, n_x, n_h, n_y, loops):
         super().__init__()
@@ -9,6 +10,9 @@ class LoopNet(nn.Module):
         self.n_h = n_h
         self.n_y = n_y
         self.loops = loops
+
+        if loops == 0:
+            raise Exception('Error: loops=0 results in y_hat=0.')
 
         self.n_l = n_x + n_h + n_y
         self.model = nn.Sequential(
@@ -27,9 +31,31 @@ class LoopNet(nn.Module):
         # Fill in first range of nodes with input
         z[:, :self.n_x] = x
 
-        for loop in range(self.loops - 1):
+        for _ in range(self.loops - 1):
             z = self.relu(self.model(z))
 
         z = self.sigmoid(self.model(z))
 
         return z[:, -self.n_y:]
+
+
+class NaiveNet(nn.Module):
+    def __init__(self, n_x, n_y, n_hs:list):
+        super().__init__()
+    
+        sizes = [n_x] + n_hs + [n_y]
+        n_l = len(sizes)
+
+        layers = []
+        for i in range(n_l - 2):
+            layers.append(nn.Linear(sizes[i], sizes[i + 1]))
+            layers.append(nn.ReLU())
+        
+        layers.append(nn.Linear(sizes[-2], sizes[-1]))
+        layers.append(nn.Sigmoid())
+
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        y_hat = self.model(x)
+        return y_hat
--- train.py
+++ train.py
@@ -5,13 +5,26 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 from dataset import xor_sampler
-from model import LoopNet
-
-epochs = 1000
-batch_size = 128
+from model import LoopNet, NaiveNet
 
 
-model = LoopNet(2, 16, 1, 3)
+epochs = 10000
+batch_size = 64
+
+n_h = 32
+n_l = 5
+n_hs = [n_h for _ in range(n_l)]
+
+model = LoopNet(
+    n_x=2,
+    n_h=sum(n_hs),
+    n_y=1,
+    loops=n_l + 1)
+
+model = NaiveNet(
+    n_x=2,
+    n_y=1,
+    n_hs=n_hs)
 
 optimizer = optim.Adam(model.parameters())
 criterion = nn.BCELoss()
